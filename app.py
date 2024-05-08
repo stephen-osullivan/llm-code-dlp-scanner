a87b1da93a15a6b6df505c48f023d26619fa12bd
stephen-osullivan <steveosulli@gmail.com>
2024-05-08 17:01:16 +0100

    first version working reasonbly well - app code needs refactoring

--- app.py
+++ app.py
@@ -9,53 +9,80 @@ import streamlit as st
 import shutil
 import os
 
-from utils import list_repo_files, load_readme_file
+from utils import list_repo, load_readme_file, load_repo_files
 
 ## prompt template
+
+system_prompt = """
+    You are a useful AI assitant, who is an expert at reviewing code repositories. 
+    Please keep your answers consise and strive to answer the users questions exactly."""
 prompt = ChatPromptTemplate.from_messages(
     [
-        ('system', "You are a useful and attentive assitant. Please respond to user queries."),
+        ('system', system_prompt),
         ("human", "{question}"),
     ]
 )
 
 ## model
-llm = ollama.Ollama(model='llama3', stop=['<|eot_id|>'], num_ctx = 2048)
+llm = ollama.Ollama(model='llama3', stop=['<|eot_id|>'], num_ctx = 2048, temperature=0.2)
 output_parser = StrOutputParser()
 chain = prompt|llm|output_parser
 
 ## streamlit
 st.set_page_config(page_title="Repo Exploration", layout="wide")
 st.title('Repo Exploration')
-repo_local_path=None
+repo_local_path = None
 with st.sidebar:
-    repo_url = st.text_input('Please enter a github repo url: https://github.com/user/repo.git')
+    repo_type = st.selectbox('Repo Type', ['Local', 'Online'])
+    if repo_type == 'Online':
+        repo_url = st.text_input('Please enter a github repo url: https://github.com/user/repo.git')
+        st.write(f'Repo: {repo_url}')
+        if repo_url:
+            download_repo = st.button('Download Repo')
+            repo_local_path = os.path.join('repos', '/'.join(repo_url.split('/')[-2:]).split('.git')[0])
+            if download_repo:
+                # Clone the repository
+                if os.path.exists(repo_local_path):
+                    # Remove the existing directory and its contents
+                    shutil.rmtree(repo_local_path)
+                repo = Repo.clone_from(repo_url, repo_local_path)
+    elif repo_type == 'Local':
+        repo_local_path = st.text_input('Enter Repo Directory')
+        st.write(f'Repo: {repo_local_path}')
 
-    if repo_url:
-        download_repo = st.button('Download Repo')
-        repo_local_path = os.path.join('repos', '/'.join(repo_url.split('/')[-2:]).split('.git')[0])
-        if download_repo:
-            # Clone the repository
-            if os.path.exists(repo_local_path):
-                # Remove the existing directory and its contents
-                shutil.rmtree(repo_local_path)
-            repo = Repo.clone_from(repo_url, repo_local_path)
-    
 if repo_local_path:
-    st.write(f'Repo: {repo_url}')
-    col1, col2 = st.columns(2)
+    if st.button('Load Repo Files'):  
+        repo_docs = load_repo_files(repo_local_path=repo_local_path)
+        prompt = """
+        Please look for confidential information leaks in the below file. I'd like you to identify passwords, api_keys, or PII customer information like DOBs, addresses, and names. 
+        Please only mention leaks that have you have actually discovered and do not give general suggestions on how to avoid leaks.
+        Can you also summarise what the files are doing please in one sentence.
+        """
+        
+        for doc in repo_docs:
+            file_name = doc.metadata['source']
+            invoke_args = {'question': prompt + '\n\n' + '#'*20 + '\n' + file_name + '#'*20 +'\n\n'+ doc.page_content}
+            st.write(f'**{file_name}**:\n\n{chain.invoke(invoke_args)}')
+
+
+    readme_file_string = load_readme_file(repo_local_path)
+
+    col1, col2 = st.columns([2,1])
     with col1:
         if st.toggle('Show README.md'):
-            st.markdown('# README File:')
-            st.write(load_readme_file(repo_local_path),)
+            st.markdown(readme_file_string, unsafe_allow_html=True)
+
     with col2:
         if st.toggle('Show Repo Files'):
-            st.markdown('# Repo File Structure')
-            repo_files = list_repo_files(repo_local_path)
-            print(repo_files)
-            st.write('\n'.join([f"* {d['file_path']}: {d['file_size']} Bytes" for d in repo_files]))
+            repo_list = list_repo(repo_local_path)
+            repo_files = [d for d in repo_list if d['blob_type'] =='blob']
+            repo_dirs = [d for d in repo_list if d['blob_type'] =='tree']
+            out_string = '**Repo Files:**\n\n'
+            out_string = out_string + ", ".join([f"{d['file_path']}: {d['file_size']/1024:.2f} KB" for d in repo_files])
+            out_string = out_string + '\n\n**Repo Dirs:**\n\n' + '\n'.join([d['file_path'] for d in repo_dirs])
+            st.write(out_string)
 
-if repo_url:
+if repo_local_path:
     input_text = st.text_input("Please enter query")
     if input_text:
         st.write(chain.invoke({'question':input_text}))
--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 from git import repo, Repo
-
+import streamlit as st
 import os
 
+st.cache_data
 def load_readme_file(directory):
     readme_path = os.path.join(directory, "README.md")
     if os.path.exists(readme_path):
@@ -10,15 +11,39 @@ def load_readme_file(directory):
         return readme_content
     else:
         return "No README file found in the specified directory."
-
-def list_repo_files(repo_local_path, commit_hash='HEAD'):
+st.cache_data
+def list_repo(repo_local_path, commit_hash='HEAD', depth = 1, files_only=False):
     commit = Repo(repo_local_path).commit(commit_hash)
     tree = commit.tree
     output_list = []
     # Traverse the tree and list files with their sizes
-    for blob in tree.traverse():
-        if blob.type == "blob":
-            file_path = os.path.join(repo_local_path, blob.path)
-            file_size = os.path.getsize(file_path)
-            output_list.append({'file_path' : file_path, 'file_size': file_size})
+    for blob in tree.traverse(depth=depth):
+        file_path = os.path.join(repo_local_path, blob.path)
+        file_size = os.path.getsize(file_path)
+        if files_only and blob.type =='tree':
+            continue
+        output_list.append({'blob_type': blob.type, 'file_path' : blob.path, 'file_size': file_size})
     return output_list
+
+st.cache_data
+def load_repo_files(repo_local_path, depth=-1, max_size = 10*1024):
+    from langchain_community.document_loaders import TextLoader
+
+    # Load the changed files into LangChain documents
+    documents = []
+    file_dicts = list_repo(repo_local_path, depth=depth, files_only=True)
+    for file_dict in file_dicts:
+        file_path, file_size = file_dict['file_path'], file_dict['file_size']
+        full_path = os.path.join(repo_local_path, file_path)
+        if (file_size < max_size): # less than 10kb 
+            try:
+                loader = TextLoader(full_path, autodetect_encoding=True)
+                documents.extend(loader.load())
+            except:
+                print('Failed to decode:', file_path)
+    # Now you can use the 'documents' list with LangChain
+    print(f"Loaded {len(documents)} documents from the changed files.")
+    doc_names = [doc.metadata['source'] for doc in documents]
+    print('Lengths:', [(n, len(doc.page_content)) for n, doc in zip(doc_names, documents)])
+    return documents
+    
